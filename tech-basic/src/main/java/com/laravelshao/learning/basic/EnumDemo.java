d04d4523ced74cb1bdb42b43f1860d466eaf6ba7
laravelshao <laravelshao@163.com>
2019-08-25 21:22:47 +0800

    update

--- tech-basic/src/main/java/com/laravelshao/learning/basic/EnumDemo.java
+++ tech-basic/src/main/java/com/laravelshao/learning/basic/EnumDemo.java
@@ -2,9 +2,11 @@ package com.laravelshao.learning.basic;
 
 /**
  * 枚举说明：
- * 枚举实际是 final class
- * 枚举成员修饰符为 public static final
- * values() 是 Java 编译器实现的字节码提升
+ * 1.枚举实际是 final class
+ * 2.枚举成员为常量，类型为当前类型，修饰符为 public static final
+ * 3.values() 是 Java 编译器实现的字节码提升
+ *
+ * 设计：枚举可以存在抽象方法 {@link java.util.concurrent.TimeUnit}
  *
  * @author qinghua.shao
  * @date 2017/8/25
