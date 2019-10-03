3cc6b836b6d4cab07d5129200a66963fff9ee496
laravelshao <laravelshao@163.com>
2019-10-03 19:35:26 +0800

    添加 elastic-search、utils 模块

---
+++ tech-utils/src/main/java/com/laravelshao/learning/utils/collections/ListUtilsTest.java
@@ -0,0 +1,25 @@
+package com.laravelshao.learning.utils.collections;
+
+import org.apache.commons.collections.ListUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author qinghua.shao
+ * @date 2019/8/20
+ * @since 1.0.0
+ */
+public class ListUtilsTest {
+
+    public static void main(String[] args) {
+        List origin = Arrays.asList(new String[]{"1","2","3","4"});
+        List list2 = Arrays.asList(new String[]{"1","3"});
+
+        // 移除指定集合元素
+        System.out.println(ListUtils.removeAll(origin,list2));
+        // 交集
+        System.out.println(ListUtils.retainAll(origin,list2));
+    }
+
+}
---
+++ tech-utils/src/main/java/com/laravelshao/learning/utils/string/StringTest.java
@@ -0,0 +1,15 @@
+package com.laravelshao.learning.utils.string;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author qinghua.shao
+ * @date 2019/8/21
+ * @since 1.0.0
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.substringBetween("tag456tag", "tag"));
+    }
+}
