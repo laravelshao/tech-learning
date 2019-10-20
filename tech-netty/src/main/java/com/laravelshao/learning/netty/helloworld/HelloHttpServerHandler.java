0862d62317494ba1578e58276f6d319a742d200a
laravelshao <laravelshao@163.com>
2019-10-20 15:43:45 +0800

    netty 简单测试添加分阶段状态打印

--- tech-netty/src/main/java/com/laravelshao/learning/netty/helloworld/HelloHttpServerHandler.java
+++ tech-netty/src/main/java/com/laravelshao/learning/netty/helloworld/HelloHttpServerHandler.java
@@ -6,6 +6,9 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.URI;
 
 /**
  * Hello HTTP 服务端处理器
@@ -14,12 +17,21 @@ import io.netty.util.CharsetUtil;
  * @date 2019/10/13
  * @since 1.0.0
  */
+@Slf4j
 public class HelloHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
 
+        System.out.println(channelHandlerContext.channel().remoteAddress());
+
         if (httpObject instanceof HttpRequest) {
+
+            HttpRequest httpRequest = (HttpRequest) httpObject;
+            System.out.println("请求方式：" + httpRequest.method().name());
+            URI uri = new URI(httpRequest.uri());
+            System.out.println("请求路径：" + uri.getPath());
+
             ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
             DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
@@ -29,4 +41,33 @@ public class HelloHttpServerHandler extends SimpleChannelInboundHandler<HttpObje
         }
     }
 
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler added");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel registered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel active");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel inactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel unregistered");
+        super.channelUnregistered(ctx);
+    }
 }
