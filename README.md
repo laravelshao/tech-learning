42be18b675406c46fbcb21b0b3ff2068877168e3
laravelshao <laravelshao@163.com>
2019-12-08 20:48:33 +0800

    添加 netty 服务端启动源码分析

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 JDK/源码/框架/设计模式学习记录
 
 
-### Elastic Job
-- [Elastic Job 问题处理](./docs/elastic-job-troubleshooting.md)
+### Netty
+- [Netty 服务端启动源码分析](./docs/netty/netty-server-start-source.md)
 
 
 ### gRPC
-- [gRPC 问题处理](./docs/grpc-troubleshooting.md)
+- [gRPC 问题处理](./docs/grpc/grpc-troubleshooting.md)
+
+
+### Elastic Job
+- [Elastic Job 问题处理](./docs/elastic-job/elastic-job-troubleshooting.md)
--- tech-netty/src/main/java/com/laravelshao/learning/netty/socket/MySocketServer.java
+++ tech-netty/src/main/java/com/laravelshao/learning/netty/socket/MySocketServer.java
@@ -5,6 +5,8 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 /**
  * Socket 服务端
@@ -22,6 +24,7 @@ public class MySocketServer {
         try {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
                     .childHandler(new MySocketServerInitializer());
 
             ChannelFuture channelFuture = serverBootstrap.bind(8080).sync();
