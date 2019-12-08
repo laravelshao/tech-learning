495a968086db87742c35c55443fb97d01f3a6171
laravelshao <laravelshao@163.com>
2019-12-08 20:49:24 +0800

    添加 easyexcel 读取 excel 测试

---
+++ tech-utils/src/main/java/com/laravelshao/learning/utils/excel/DemoDataListener.java
@@ -0,0 +1,38 @@
+package com.laravelshao.learning.utils.excel;
+
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoDataListener extends AnalysisEventListener<DemoData> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoDataListener.class);
+
+    List<DemoData> list = new ArrayList<DemoData>();
+
+    /**
+     * 这个每一条数据解析都会来调用
+     *
+     * @param data    one row value. Is is same as {@link AnalysisContext#readRowHolder()}
+     * @param context
+     */
+    @Override
+    public void invoke(DemoData data, AnalysisContext context) {
+        LOGGER.info("解析到一条数据:{}", JSON.toJSONString(data));
+        list.add(data);
+    }
+
+    /**
+     * 所有数据解析完成了 都会来调用
+     *
+     * @param context
+     */
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext context) {
+        LOGGER.info("所有数据解析完成！");
+    }
+}
---
+++ tech-utils/src/main/java/com/laravelshao/learning/utils/excel/ExcelReadTest.java
@@ -0,0 +1,18 @@
+package com.laravelshao.learning.utils.excel;
+
+import com.alibaba.excel.EasyExcel;
+
+/**
+ * @author qinghua.shao
+ * @date 2019/11/12
+ * @since 1.0.0
+ */
+public class ExcelReadTest {
+
+    public static void main(String[] args) {
+        String fileName = "/Users/sylvia/Desktop/demo.xlsx";
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
+        EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).sheet().doRead();
+
+    }
+}
