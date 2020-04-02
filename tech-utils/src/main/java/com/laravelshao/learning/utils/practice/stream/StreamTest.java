ad55461916ea3ddb60beae2f3e8d9fd1bdbf099a
laravelshao <laravelshao@163.com>
2020-04-02 11:10:22 +0800

    stream 测试

--- tech-utils/src/main/java/com/laravelshao/learning/utils/practice/stream/StreamTest.java
+++ tech-utils/src/main/java/com/laravelshao/learning/utils/practice/stream/StreamTest.java
@@ -3,6 +3,7 @@ package com.laravelshao.learning.utils.practice.stream;
 import com.google.common.collect.Lists;
 import com.laravelshao.learning.utils.model.Coupon;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -19,26 +20,59 @@ public class StreamTest {
 
     public static void main(String[] args) {
 
+        // List中对象多属性拼接作为key
+        //list2Map();
+
+        // 排序
+        //sorted();
+
+        // 过滤指定条件数据
+        filter();
+    }
+
+    /**
+     * List => Map
+     */
+    public static void list2Map() {
+
         List<Coupon> couponList = Lists.newArrayList();
         couponList.add(new Coupon(1, "100001"));
         couponList.add(new Coupon(2, "200001"));
         couponList.add(new Coupon(3, "300001"));
-
         // 将list中对象多属性拼接作为key Map<sourceType_couponId,Coupon>
         Map<String, Coupon> couponMap = couponList.stream().collect(
                 Collectors.toMap(coupon -> coupon.getSourceType() + "_" + coupon.getCouponId(), coupon -> coupon));
-
         System.out.println(couponMap);
+    }
 
-        // 过滤指定条件数据
-        //dbCouponList.stream().filter(entity -> paramCouponMap
-        //        .containsKey(entity.getSourceType() + "_" + entity.getCouponActivityId()))
-        //        .collect(Collectors.toList());
+    /**
+     * 排序
+     */
+    public static void sorted() {
 
+        List<Coupon> couponList = Lists.newArrayList();
+        couponList.add(new Coupon(10, "100001"));
+        couponList.add(new Coupon(31, "200001"));
+        couponList.add(new Coupon(17, "300001"));
         // 指定排序
         List<Coupon> sortedCouponList = couponList.stream()
                 .sorted(Comparator.comparing(Coupon::getCouponId).reversed()).collect(Collectors.toList());
+        System.out.println(sortedCouponList);
+    }
 
+    /**
+     * 过滤指定条件数据
+     */
+    public static void filter() {
+        List<Integer> list = Arrays.asList(1, 123, 12, 18, 23, 19);
+
+        // 过滤指定条件数据
+        List<Integer> after = list.stream().filter(tmp -> tmp % 3 == 0).collect(Collectors.toList());
+        System.out.println(after);
+
+        //dbCouponList.stream().filter(entity -> paramCouponMap
+        //        .containsKey(entity.getSourceType() + "_" + entity.getCouponActivityId()))
+        //        .collect(Collectors.toList());
     }
 
 }
