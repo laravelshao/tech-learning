02defddc40d1df205d0d99d5e343411edff3681d
laravelshao <laravelshao@163.com>
2020-04-22 10:44:20 +0800

    添加 Predicate & BiPredicate 测试

--- tech-jdk/src/main/java/com/laravelshao/learning/jdk/function/PredicateTest.java
+++ tech-jdk/src/main/java/com/laravelshao/learning/jdk/function/PredicateTest.java
@@ -1,9 +1,12 @@
 package com.laravelshao.learning.jdk.function;
 
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 /**
  * Predicate 测试
+ *
+ * <p>BiPredicate 与 Predicate 的不同就是传入两个参数
  *
  * @author qinghua.shao
  * @date 2020/4/22
@@ -16,6 +19,7 @@ public class PredicateTest {
         Predicate<Integer> biggerThan6 = x -> x > 6;
         Predicate<Integer> lessThan3 = x -> x < 3;
         Predicate<Integer> lessThan9 = x -> x < 9;
+        BiPredicate<String, String> bothLengthLessThan6 = (s, s1) -> s.length() < 6 && s1.length() < 6;
         // 7比6大为true，取反 => false
         System.out.println("Predicate negate => " + biggerThan6.negate().test(7));
         // 7比6大 => true
@@ -28,5 +32,16 @@ public class PredicateTest {
         System.out.println("Predicate isEqual false => " + Predicate.isEqual("test").test("num"));
         System.out.println("Predicate isEqual true => " + Predicate.isEqual("num").test("num"));
 
+        System.out.println("BiPredicate test() => " + bothLengthLessThan6.test("hello", "helloworld"));
+    }
+
+    /**
+     * 判断给定字符串长度是否大于4
+     */
+    public class MyPredicate implements Predicate<String> {
+        @Override
+        public boolean test(String s) {
+            return s.length() > 4;
+        }
     }
 }
