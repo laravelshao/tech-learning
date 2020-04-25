4a98127b694fbf4639fd6acb15b22dd0135865c6
laravelshao <laravelshao@163.com>
2020-04-25 13:30:25 +0800

    枚举 单元测试

--- tech-jdk/src/test/java/com/laravelshao/learning/jdk/EnumTest.java
+++ tech-jdk/src/test/java/com/laravelshao/learning/jdk/EnumTest.java
@@ -1,11 +1,14 @@
-package com.laravelshao.learning.jdk.enums;
+package com.laravelshao.learning.jdk;
+
+import com.laravelshao.learning.jdk.enums.PriceHandleTypeEnum;
+import org.junit.Test;
 
 /**
  * 枚举说明：
  * 1.枚举实际是 final class
  * 2.枚举成员为常量，类型为当前类型，修饰符为 public static final
  * 3.values() 是 Java 编译器实现的字节码提升
- *
+ * <p>
  * 设计：枚举可以存在抽象方法 {@link java.util.concurrent.TimeUnit}
  *
  * @author qinghua.shao
@@ -14,7 +17,8 @@ package com.laravelshao.learning.jdk.enums;
  */
 public class EnumTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void enumTest() {
         PriceHandleTypeEnum priceHandleTypeEnum = PriceHandleTypeEnum.byCode("1");
         System.out.println(priceHandleTypeEnum.getValue());
     }
