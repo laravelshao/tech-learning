9f9d9b4ded52b48bed81722fd5dbe87bc31feeed
laravelshao <laravelshao@163.com>
2020-06-20 15:00:00 +0800

    添加 数字格式化 测试

---
+++ tech-utils/src/main/java/com/laravelshao/learning/utils/practice/number/NumberTest.java
@@ -0,0 +1,28 @@
+package com.laravelshao.learning.utils.practice.number;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+/**
+ * @author qinghua.shao
+ * @date 2020/6/19
+ * @since 1.0.0
+ */
+public class NumberTest {
+
+    public static void main(String[] args) {
+        // 设置小数格式为仅保留尾部非0小数
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        //DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        System.out.println(decimalFormat.format(10.00));
+        System.out.println(decimalFormat.format(10.01));
+        System.out.println(decimalFormat.format(10.050));
+        System.out.println(decimalFormat.format(10.059));
+
+
+        System.out.println(decimalFormat.format(new BigDecimal(10.00)));
+        System.out.println(decimalFormat.format(new BigDecimal(10.01)));
+        System.out.println(decimalFormat.format(new BigDecimal(10.050)));
+        System.out.println(decimalFormat.format(new BigDecimal(10.059)));
+    }
+}
