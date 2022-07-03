00d0727b875fdd98665d8769ce2be64631bdb428
laravelshao <laravelshao@163.com>
2022-07-03 13:49:56 +0800

    添加 LockSupport park 测试

---
+++ tech-concurrent/src/main/java/com/laravelshao/learning/concurrent/lock/ParkTest.java
@@ -0,0 +1,48 @@
+package com.laravelshao.learning.concurrent.lock;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * park 测试
+ * <p>
+ * LockSupport#park()：挂起当前线程并阻塞
+ * LockSupport#unpark(thread1)：唤醒指定线程
+ *
+ * @author qinghua.shao
+ * @date 2022/7/3
+ * @since 1.0.0
+ */
+public class ParkTest {
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread() {
+            @Override
+            public void run() {
+
+                System.out.println("线程1挂起之前执行的操作");
+                LockSupport.park();
+                System.out.println("线程1被唤醒后执行的操作");
+
+            }
+        };
+        thread1.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+
+                for (int i = 1; i <= 10; i++) {
+                    try {
+                        Thread.sleep(1000);
+                        System.out.println("等待 " + i + " 秒");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("尝试唤醒线程1");
+                LockSupport.unpark(thread1);
+            }
+        }.start();
+    }
+}
