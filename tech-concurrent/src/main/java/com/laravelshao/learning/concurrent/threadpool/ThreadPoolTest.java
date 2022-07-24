e3a543cfbee6661fa94849449842bd01970a98cb
laravelshao <laravelshao@163.com>
2022-07-24 18:42:58 +0800

    添加 线程池 测试

--- tech-concurrent/src/main/java/com/laravelshao/learning/concurrent/threadpool/ThreadPoolTest.java
+++ tech-concurrent/src/main/java/com/laravelshao/learning/concurrent/threadpool/ThreadPoolTest.java
@@ -4,15 +4,29 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * @author shaoqinghua
+ * 线程池测试
+ *
+ * @author qinghua.shao
  * @date 2019/2/16
- * @description
+ * @since 1.0.0
  */
 public class ThreadPoolTest {
 
     public static void main(String[] args) {
 
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
-        //Executors.newScheduledThreadPool()
+        // 固定线程数线程池
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(5);
+        // 单个线程线程池
+        ExecutorService singleThreadPool = Executors.newSingleThreadExecutor();
+        // 可根据需要创建新线程的线程池
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        // 定时执行任务的线程池
+        ExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+
+        fixedThreadPool.execute(() -> System.out.println("线程任务"));
+
+        singleThreadPool.execute(() -> System.out.println("线程任务"));
+
+        cachedThreadPool.execute(() -> System.out.println("线程任务"));
     }
 }
