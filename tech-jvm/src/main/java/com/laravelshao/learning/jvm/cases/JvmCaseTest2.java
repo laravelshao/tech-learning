804130da33bc6d3fb059f9397b7cdeae5b83496a
laravelshao <laravelshao@163.com>
2023-11-05 12:35:50 +0800

    jvm 调优测试 fix

--- tech-jvm/src/main/java/com/laravelshao/learning/jvm/cases/JvmCaseTest2.java
+++ tech-jvm/src/main/java/com/laravelshao/learning/jvm/cases/JvmCaseTest2.java
@@ -3,7 +3,7 @@ package com.laravelshao.learning.jvm.cases;
 /**
  * 案例分析：模拟每日百亿数据量实时分析引擎定位和处理频繁Full GC
  *
- * <p>JVM Param: -Xms20m -Xmx20m -Xmn10m -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15 -XX:PretenureSizeThreshold=20971520 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps
+ * <p>JVM Param: -Xms200m -Xmx200m -Xmn100m -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15 -XX:PretenureSizeThreshold=20971520 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps
  *
  * @author qinghua.shao
  * @date 2022/6/11
@@ -11,6 +11,12 @@ package com.laravelshao.learning.jvm.cases;
  */
 public class JvmCaseTest2 {
 
+    /**
+     * 模拟说明：
+     * 每秒种都会执行一次 loadData() 方法，会分配4个10MB的数组，但是都立马成为垃圾；但是会有 data1 和 data2两个10MB
+     * 的数组是被变量引用必须存活的，此时 Eden 区已经占用60MB，接着 data3 变量一次指向两个10MB的数组，从而在1秒内触发
+     * Young GC。
+     */
     public static void main(String[] args) throws InterruptedException {
         Thread.sleep(30000);
         while (true) {
